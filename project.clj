aac4b3b80105cc6cdd6fafe6fd4a0effa76386db
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2018-03-25 20:50:27 +0300

    Bump to Clojure 1.9.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure bindings for AWS"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-http "3.7.0"]
                  [cheshire "5.7.1"]
                  [net.cgrand/xforms "0.7.2"]
