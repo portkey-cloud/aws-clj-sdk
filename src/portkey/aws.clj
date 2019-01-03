65764a7056e08ece94b3a2e636de9f6431e0293a
Baptiste Dupuch <baptiste.dupuch@gmail.com>
2019-01-03 10:11:45 +0100

    Split utils and rest-xml tests

--- src/portkey/aws.clj
+++ src/portkey/aws.clj
@@ -267,7 +267,7 @@
                                                                                                          (sequence (keep (fn [{:http.request.field/keys [xml-attribute] :as val}]
                                                                                                                            (and xml-attribute val)))
                                                                                                                    value))
-                                                                       (= type "map" (throw (Exception. "AWS map type is not handled as we never saw any cases in xml body from rest-xml protocol.")))
+                                                                       (= type "map")       (throw (Exception. "AWS map type is not handled as we never saw any cases in xml body from rest-xml protocol."))
                                                                        (= type "list")      (if flattened
                                                                                               ;; if list is flattened, then we ignore root tag
                                                                                               (into [] (map #(map->xml % location-name) value))
